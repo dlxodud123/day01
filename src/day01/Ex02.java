a86ce956525173b1c75f829ecb09049e413ffdab
taeyoung <xodud5080@naver.com>
2024-02-05 16:51:57 +0900

    올라가라

---
+++ src/day01/Ex02.java
@@ -0,0 +1,15 @@
+package day01;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		System.out.println("100\n200\n300");
+		System.out.println("1\t1234567\t12345678\t1");
+		System.out.println("이름\t나이");
+		System.out.println("=============");
+		System.out.println("홍길동\t20살");
+		System.out.println("김개똥\t30살");
+		System.out.println("-------------");
+		System.out.println(" \\ \" ");
+		System.out.println("\"C:\\0.공유자료\\주말-플젝반 학생관리\"");
+	}
+}
---
+++ src/day01/Ex03.java
@@ -0,0 +1,15 @@
+package day01;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		System.out.println(100 + 100);
+		System.out.println(1.123 + 3.456);
+		
+		System.out.println("안녕하세요" + " 이태영님");
+		System.out.println("100 " + 200);
+		System.out.println("백" + 100);
+		
+		System.out.println("안녕" + 100 + 100);
+		System.out.println("안녕" + (100 + 100));
+	}
+}
